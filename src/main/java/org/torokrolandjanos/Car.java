f7185f2ee24488dc50955d7dbd59dc3e4cc769ae
Torok Roland <torok.roland1998@gmail.com>
2023-03-06 18:41:38 +0200

    added Car class;

---
+++ src/main/java/org/torokrolandjanos/Car.java
@@ -0,0 +1,15 @@
+package org.torokrolandjanos;
+
+public class Car {
+
+    //instance variables
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+}
